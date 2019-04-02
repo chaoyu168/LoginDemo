2d49380184cb0f4d9861a35412a15eb743671966
chaoyu168 <531501604@qq.com>
2019-04-02 16:01:46 +0800

    first commit

---
+++ app/src/main/java/com/xiaobai/logindemo/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.xiaobai.logindemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+
+/**
+ * Created by Administrator on 2019/4/2
+ * <p>
+ * desc:
+ */
+public class HomeActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
